d96413a2b05516454ecd48e48fbc10a55dc62bb8
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-05-20 14:34:35 -0700

    Add another gradient check for the network forward/backward passes

--- assignment1/q2_neural.py
+++ assignment1/q2_neural.py
@@ -99,7 +99,18 @@ def your_sanity_checks():
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    N = 200
+    dimensions = [10, 5, 10, 10, 10, 100, 100, 100, 100, 100]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in xrange(N):
+        labels[i, random.randint(0,dimensions[2]-1)] = 1
+
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+
+    gradcheck_naive(lambda params:
+        forward_backward_prop(data, labels, params, dimensions), params)
     ### END YOUR CODE
 
 
