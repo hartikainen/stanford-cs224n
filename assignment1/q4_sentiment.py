35e992ae3bff44264053eb4c1bfccc3d44cb96c7
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-05-24 22:38:43 -0700

    q4 code and plots
    * Adds getSentenceFeatures, getRegularizationValues, and
      chooseBestModel for q4_sentiment.py
    * Adds confusion matrix and regularization plots for the q4 runs

--- assignment1/q4_sentiment.py
+++ assignment1/q4_sentiment.py
@@ -49,7 +49,8 @@ def getSentenceFeatures(tokens, wordVectors, sentence):
     sentVector = np.zeros((wordVectors.shape[1],))
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    word_idx = [tokens[word] for word in sentence]
+    sentVector = np.sum(wordVectors[word_idx], axis=0)
     ### END YOUR CODE
 
     assert sentVector.shape == (wordVectors.shape[1],)
@@ -63,7 +64,7 @@ def getRegularizationValues():
     """
     values = None   # Assign a list of floats in the block below
     ### YOUR CODE HERE
-    raise NotImplementedError
+    values = [np.power(10.0, p) for p in np.arange(-6, 10)]
     ### END YOUR CODE
     return sorted(values)
 
@@ -84,10 +85,8 @@ def chooseBestModel(results):
     Returns:
     Your chosen result dictionary.
     """
-    bestResult = None
-
     ### YOUR CODE HERE
-    raise NotImplementedError
+    bestResult = max(results, key=lambda result: result["dev"])
     ### END YOUR CODE
 
     return bestResult
